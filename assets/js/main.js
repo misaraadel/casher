cc54b950c5f907d67457eb0ceaa8ad2f48887c62
misaraadel <41232116+misaraadel@users.noreply.github.com>
2022-02-23 17:03:22 +0200

    add casher page

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,4 +2,8 @@ $(document).ready(function () {
     $('#hidden-section').click(function(){
         $('.hidden-content').toggleClass('active-content');
     });
+
+    $('.open-menu').click(function(){
+        $('.menu-list').addClass('active-list');
+    });
 });
