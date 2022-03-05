3a7e32e58d09014ee262147372a5e99cbc544249
misaraadel <41232116+misaraadel@users.noreply.github.com>
2022-03-05 17:41:17 +0200

    add reports pages and dark mode

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,4 +6,34 @@ $(document).ready(function () {
     $('.open-menu').click(function(){
         $('.menu-list').addClass('active-list');
     });
+
+    let theme = localStorage.getItem('theme');
+    theme === 'dark' ? enableDarkMode() : disableDarkMode();
+
+    function enableDarkMode() {
+        localStorage.setItem('theme', 'dark');
+        document.body.classList.add('dark-mode');
+        $('.darkTheme').addClass('active');
+        $('.theme').addClass('darkmode');
+    }
+
+    function disableDarkMode() {
+        localStorage.setItem('theme', 'light');
+        document.body.classList.remove('dark-mode');
+        $('.lightTheme').addClass('active');
+        $('.theme').removeClass('darkmode');
+    }
+
+    $('.darkTheme').on('click', () => {
+        enableDarkMode();
+        console.log('Dark Btn  ', theme);
+        // $('.themeBtn').removeClass('active');
+        // $(this).addClass('active');
+    });
+
+    $('.lightTheme').on('click', () => {
+        disableDarkMode();
+        console.log('light Btn', theme);
+    });
+
 });
